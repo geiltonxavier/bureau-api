5c9298fcb4aa92bb9085a305272e831103bfe8bf
Geilton Xavier <geilton@vizir.com.br>
2019-07-10 11:14:51 -0300

    add models Product and Category

---
+++ Bureau.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bureau.API.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
