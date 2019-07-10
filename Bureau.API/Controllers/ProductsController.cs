3fbddb0069a19ebe6f51aa84d3584f058c0e6cd7
Geilton Xavier <geilton@vizir.com.br>
2019-07-10 13:51:30 -0300

    Add Products Controller

---
+++ Bureau.API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Bureau.API.Models;
+
+namespace Bureau.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly BureauContext _context;
+
+        public ProductsController(BureauContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.Include(p => p.Category).ToListAsync();
+        }       
+    }
+}
