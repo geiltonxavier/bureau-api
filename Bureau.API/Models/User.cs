1e1d8ed9128038542a352536b1b611194dc386a2
Geilton Xavier <geilton@vizir.com.br>
2019-07-10 11:16:25 -0300

    add user model

---
+++ Bureau.API/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bureau.API.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
